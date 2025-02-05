e2793eb30627517c4fff001a3bf2c42203ad0ce4
Minf-afk <msantana.inf@gmail.com>
2025-02-05 09:44:17 -0300

    ajustes

---
+++ prototype/funcao-construtora.js
@@ -0,0 +1,12 @@
+function Pessoa (nome,idade){
+    this.nome = nome
+    this.idade = idade
+}
+
+Pessoa.prototype.falar = function(){
+    console.log(`Meu nome é: ${this.nome}`)
+}
+
+const mateus = new Pessoa('mateus', 23)
+
+mateus.falar();
