ac1ecefd8df370d625ff2effb23868e4446f1de6
Minf-afk <msantana.inf@gmail.com>
2025-02-05 12:10:19 -0300

    ajustes

---
+++ funcoes/funcoes.js
@@ -0,0 +1,10 @@
+function apenasUmaFuncao() {
+  console.log("apenasUmaFuncao");
+}
+
+const outraFuncao = function () {
+  console.log("outraFuncao");
+};
+
+apenasUmaFuncao();
+outraFuncao();
--- proto/objeto.js
+++ proto/objeto.js
@@ -3,4 +3,11 @@ function Pessoa (nome, idade){
     this.idade = idade
 }
 
+Pessoa.prototype.falar = function(){
+    console.log(`Meu nome é: ${this.nome}`)
+}
+
+const mateus = new Pessoa('mateus', 23);
+mateus.falar();
+
 console.log(new Pessoa('mateus', 23));
